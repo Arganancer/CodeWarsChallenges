31b153dbf04807257f0367567062de0b60534940
Jean-Raphael Auclair-Soucy <Arganancer@gmail.com>
2020-12-01 10:23:28 -0500

    Completed 2 new challenges.

---
+++ Code/Completed/4 Kyu/StripCommentsSolution.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+/// <summary>
+/// https://www.codewars.com/kata/51c8e37cee245da6b40000bd/train/csharp
+/// </summary>
+public class StripCommentsSolution
+{
+	public static string StripComments( string text, string[] commentSymbols )
+	{
+		return string.Join( "\n", text.Split( "\n" ).Select( t =>
+		{
+			int i = t.IndexOfAny( commentSymbols.SelectMany( s => s.ToCharArray() ).ToArray() );
+			return (i >= 0 ? t.Substring( 0, i ) : t).TrimEnd();
+		} ) );
+	}
+}
---
+++ Code/Completed/5 Kyu/FirstNonRepeatingCharacter.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq;
+
+public class FirstNonRepeatingCharacter
+{
+	public static string FirstNonRepeatingLetter( string s )
+	{
+		string result = s.FirstOrDefault( a => s.Count( b => char.ToLower( a ) == char.ToLower( b ) ) == 1 ).ToString();
+		return result == "\0" ? "" : result;
+	}
+}
