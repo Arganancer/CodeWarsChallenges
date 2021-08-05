6664cee1182a8744ce92b9d53c016ff9bee9da34
Jean-Raphael Auclair-Soucy <jrsoucy@eddyfi.com>
2021-08-05 10:32:02 -0400

    Completed TrafficJam Kata.

---
+++ Code/Completed/5 Kyu/TrafficJam.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class TrafficJamKata
+{
+	public static string TrafficJam( string mainRoad, string[] sideStreets )
+	{
+		List<char> output = mainRoad.ToList();
+		for (int i = sideStreets.Length - 1; i >= 0; --i)
+		{
+			for (int j = 0; j < sideStreets[i].Length; ++j)
+			{
+				int insertIndex = i + (j * 2) + 1;
+				if (insertIndex > output.Count)
+				{
+					output.Add( sideStreets[i][^(j + 1)] );
+				}
+				else
+				{
+					output.Insert( insertIndex, sideStreets[i][^(j + 1)] );
+				}
+			}
+		}
+
+		return string.Join( "", output.Take( output.IndexOf( 'X' ) + 1 ) );
+	}
+}
